a1526b648423ef05ee39ce6bf3f7612ff15e78bb
Andrea Zucchini García <azga84@gmail.com>
2018-12-07 23:20:58 +0100

    Refactoring methods to add, remove and list paths of the database

--- cmds/fs_backup/main.go
+++ cmds/fs_backup/main.go
@@ -57,49 +57,78 @@ func main() {
 	firstNonFlagArgument := args[0]
 	switch strings.ToLower(firstNonFlagArgument) {
 	case list:
-		var path path
-		err := col.ForEach(func(i int, data []byte) bool {
-			err := json.Unmarshal(data, &path)
-			if err != nil {
-				fatalErr = err
-				return true
-			}
-			fmt.Printf("= %s\n", path)
-			return false
-		})
-		fatalErr = err
+		fatalErr = listPaths(col)
 		return
 	case add:
-		if len(args[1:]) == 0 {
+		paths := args[1:]
+		if len(paths) == 0 {
 			fatalErr = errors.New("must specify path to add")
 			return
 		}
-		for _, p := range args[1:] {
-			path := &path{Path: p, Hash: "Not yet archived"}
-			if err := col.InsertJSON(path); err != nil {
-				fatalErr = err
-				return
-			}
-			fmt.Printf("+ %s\n", path)
-		}
+		fatalErr = addingPaths(col, paths)
+		return
 	case remove:
-		var path path
-		err := col.RemoveEach(func(i int, data []byte) (removed bool, stop bool) {
-			err := json.Unmarshal(data, &path)
-			if err != nil {
-				fatalErr = err
-				return false, true
-			}
-
-			for _, p := range args[1:] {
-				if path.Path == p {
-					fmt.Printf("- %s\n", path)
-					return true, false
-				}
-			}
-			return false, false
-		})
-		fatalErr = err
+		paths := args[1:]
+		if len(paths) == 0 {
+			fatalErr = errors.New("must specify path to remove")
+			return
+		}
+		fatalErr = removingPaths(col, paths)
 		return
 	}
+}
+
+func removingPaths(col *filedb.C, paths []string) error {
+	var fatalErr error
+	var path path
+	err := col.RemoveEach(func(i int, data []byte) (removed bool, stop bool) {
+		err := json.Unmarshal(data, &path)
+		if err != nil {
+			fatalErr = err
+			return false, true
+		}
+
+		for _, p := range paths {
+			if path.Path == p {
+				fmt.Printf("- %s\n", path)
+				return true, false
+			}
+		}
+		return false, false
+	})
+	if err != nil {
+		fatalErr = err
+	}
+	return fatalErr
+}
+
+func addingPaths(col *filedb.C, paths []string) error {
+	var fatalErr error
+	for _, p := range paths {
+		path := &path{Path: p, Hash: "Not yet archived"}
+		if err := col.InsertJSON(path); err != nil {
+			fatalErr = err
+			break
+		}
+		fmt.Printf("+ %s\n", path)
+	}
+	return fatalErr
+}
+
+func listPaths(col *filedb.C) error {
+	var fatalErr error
+	var path path
+	err := col.ForEach(func(i int, data []byte) bool {
+		err := json.Unmarshal(data, &path)
+		if err != nil {
+			fatalErr = err
+			return true
+		}
+		fmt.Printf("= %s\n", path)
+		return false
+	})
+	if err != nil {
+		fatalErr = err
+	}
+	return fatalErr
 }
